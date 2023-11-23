a97bfb29f358c89e3fc55f49e1b28edddc190124
Dusty Miller <dusty.miller109@gmail.com>
2023-11-23 18:11:00 -0500

    Learned about slices

---
+++ slice/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let my_string  = String::from("hello world");
+
+    // 'first_word' works on slices of `String`s, whether partial or whole
+
+    let word = first_word(&my_string[0..6]);
+    println!("{word}");
+    let word = first_word(&my_string[..]);
+    println!("{word}");
+
+    let word = first_word(&my_string);
+    println!("{word}");
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    println!("{word}");
+    let word = first_word(&my_string_literal[..]);
+    println!("{word}");
+
+    let word = first_word(my_string_literal);
+    println!("{word}");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
